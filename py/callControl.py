b616c8198f45fee22793b56b46f35a279ade81ac
Stephen Simpson <stephen@sjs.io>
2019-06-11 16:04:43 -0500

    autopep8'd the file

--- py/callControl.py
+++ py/callControl.py
@@ -1,55 +1,69 @@
 from twilio.twiml.voice_response import VoiceResponse, Say, Gather, Play, Dial
 from re import match
 
-languages = {1: "Spanish", 2: "Chinese", 3: "Korean", 4: "French"} #We should move this to the config file
+languages = {1: "Spanish", 2: "Chinese", 3: "Korean",
+             4: "French"}  # We should move this to the config file
 
-#These paramaters come from the arguments within a GET request: To, Digits, From
+# These paramaters come from the arguments within a GET request: To, Digits, From
+
 
 def initialCall(To, isLive, isDelayLive):
-    if To == "+18445051150" and isLive == True: #If calling live number and is live
+    if To == "+18445051150" and isLive == True:  # If calling live number and is live
         response = VoiceResponse()
-        gather = Gather(action="/gatherDigit", method="GET", num_digits=1, timeout=30)
+        gather = Gather(action="/gatherDigit", method="GET",
+                        num_digits=1, timeout=30)
         gather.play('http://twilio.willowcreek.org/mp3/LiveList.mp3')
         response.append(gather)
         return response
-    elif To == "+18445051151" and isDelayLive == True: #If calling delayed number and is delay live
+    elif To == "+18445051151" and isDelayLive == True:  # If calling delayed number and is delay live
         response = VoiceResponse()
-        gather = Gather(action="/gatherDigit", method="GET", num_digits=1, timeout=30)
+        gather = Gather(action="/gatherDigit", method="GET",
+                        num_digits=1, timeout=30)
         gather.play('http://twilio.willowcreek.org/mp3/DelayedList.mp3')
         response.append(gather)
         return response
     else:
         response = VoiceResponse()
-        response.say('The language interpretation lines for the GLS are not yet open. Please call back 15 minutes prior to the next session')
-        
+        response.say(
+            'The language interpretation lines for the GLS are not yet open. \
+            Please call back 15 minutes prior to the next session')
+
         return response
 
-def gatherDigits(To,Digit):
-    #If they pressed a correct option
+
+def gatherDigits(To, Digit):
+    # If they pressed a correct option
     try:
-        #This if statement generates the MP3 announement file on the fly so we can avoid having to build a seperate dictionary
-        if To == "+18445051150": #These numbers need to be moved to the config file
-            mp3 = "http://twilio.willowcreek.org/mp3/{}.mp3".format("Live" + languages[Digit])
+        # This if statement generates the MP3 announement file on the fly so we can avoid having to build a seperate dictionary
+        if To == "+18445051150":  # These numbers need to be moved to the config file
+            mp3 = "http://twilio.willowcreek.org/mp3/{}.mp3".format(
+                "Live" + languages[Digit])
         else:
-            mp3 = "http://twilio.willowcreek.org/mp3/{}.mp3".format("Delayed" + languages[Digit])
-        #This will generate the wait URL, the function is conferenceOnHold
-        #After looking at this, I dont think we need it actually...
+            mp3 = "http://twilio.willowcreek.org/mp3/{}.mp3".format(
+                "Delayed" + languages[Digit])
+        # This will generate the wait URL, the function is conferenceOnHold
+        # After looking at this, I dont think we need it actually...
         if To == "+18445051150":
-            waitUrl = "http://twilio.willowcreek.org/wait/{}".format(languages[Digit]).lower() #This URL will be different
+            # This URL will be different
+            waitUrl = "http://twilio.willowcreek.org/wait/{}".format(
+                languages[Digit]).lower()
         else:
-            waitUrl = "http://twilio.willowcreek.org/wait/{}delay".format(languages[Digit]).lower() #This URL will be different
+            # This URL will be different
+            waitUrl = "http://twilio.willowcreek.org/wait/{}delay".format(
+                languages[Digit]).lower()
         # And finally the conference name
         if To == "+18445051150":
             conferenceName = languages[Digit]
         else:
             conferenceName = "{}delay".format(languages[Digit])
 
-        #Build TWIML
+        # Build TWIML
         response = VoiceResponse()
         play = Play(mp3)
         response.append(play)
         dial = Dial()
-        dial.conference(muted=True, beep=False, end_conference_on_exit=False, start_conference_on_enter=False, max_participants=250, trim="do_not_trim", wait_url=waitUrl, name=conferenceName)
+        dial.conference(muted=True, beep=False, end_conference_on_exit=False, start_conference_on_enter=False,
+                        max_participants=250, trim="do_not_trim", wait_url=waitUrl, name=conferenceName)
         response.append(dial)
 
         return response
@@ -57,25 +71,28 @@ def gatherDigits(To,Digit):
         pass
         response = VoiceResponse()
         response.say("That is not a valid option")
-        response.redirect("/hello") #this should take you back to the first URL and function initialCall
+        # this should take you back to the first URL and function initialCall
+        response.redirect("/hello")
 
         return response
+
 
 def conferenceOnHold():
     response = VoiceResponse()
     response.play("http://twilio.willowcreek.org/Hold.mp3", loop=0)
     return response
 
+
 def codecConference(From):
     conferenceName = match("sip:(.*)@.*", From).group(1)
 
     response = VoiceResponse()
     dial = Dial()
-    dial.conference(muted=False, beep=False, end_conference_on_exit=False, start_conference_on_enter=False, max_participants=250, trim="do_not_trim", name=conferenceName)
+    dial.conference(muted=False, beep=False, end_conference_on_exit=False,
+                    start_conference_on_enter=False, max_participants=250, trim="do_not_trim", name=conferenceName)
     response.append(dial)
 
     return response
-
 
 
 print(codecConference("sip:blah@blah.com"))
