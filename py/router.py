87faa57ea2ebcd6ce406902c5432b06e152d53b3
Stephen Simpson <stephen@sjs.io>
2019-06-13 15:44:03 -0500

    Ceated flask File

---
+++ py/router.py
@@ -0,0 +1,22 @@
+from flask import Flask, Response
+import callControl
+
+app = Flask(__name__)
+
+# print(callControl.initialCall(To="+18445051150",isLive=True,isDelayLive=True))
+
+@app.route('/conference/join')
+def join():
+    return Response(callControl.initialCall(To="+18445051150",isLive=True,isDelayLive=True), mimetype='text/xml')
+
+@app.route('/conference/gatherDigit')
+def gatherDigit():
+    return Response(callControl.gatherDigits(To="+18445051150",Digit=6), mimetype='text/xml')
+
+@app.route('/conference/onHold')
+def onHold():
+    return Response(callControl.conferenceOnHold(), mimetype='text/xml')
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
