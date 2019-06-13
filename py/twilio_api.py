69ef7da0c5490029806def6146d997ad4e09da44
Karl Swanson <karlcswanson@gmail.com>
2019-06-13 16:04:18 -0500

    Twilio API - Check on room size and codec status.

--- py/twilio_api.py
+++ py/twilio_api.py
@@ -1,19 +1,68 @@
+
+
 from twilio.rest import Client
+from twilio.rest.api.v2010.account.conference import ConferenceInstance
 
 import config
 
 
+class TwilioConnection:
+    def __init__(self):
+        account_sid = config.config_tree['twilio']['account_sid']
+        auth_token = config.config_tree['twilio']['auth_token']
 
-config.config()
+        self.client = Client(account_sid, auth_token)
 
-account_sid = config.config_tree['twilio']['account_sid']
-auth_token = config.config_tree['twilio']['auth_token']
-client = Client(account_sid, auth_token)
+    def open_conferences(self):
+        conference_list = []
 
-conferences = client.conferences.list(limit=20)
+        for conference in self.client.conferences.list(limit=20):
+            if conference.status in ['init', 'in-progress']:
+                conference_list.append(conference)
 
-for conference in conferences:
-    if conference.status in ['init', 'in-progress']:
-        participants = conference.participants.list()
-        participant_count = len(participants) - 1
-        print('Conference: {} on call: {}'.format(conference.friendly_name, participant_count))
+        return conference_list
+
+
+
+
+# ConferenceInstance methods
+def close(self):
+    self.update(status = 'completed')
+
+def call_count(self):
+    return len(self.participants.list())
+
+def codec_in_conference(self):
+    for p in self.participants.list():
+        if p.muted == False:
+            return True
+    return False
+
+
+
+
+
+def main():
+    ConferenceInstance.close = close
+    ConferenceInstance.call_count = call_count
+    ConferenceInstance.codec_in_conference = codec_in_conference
+
+
+    config.config()
+    conn = TwilioConnection()
+
+    conferences = conn.open_conferences()
+
+    for i in conferences:
+        print('Conference: {}, participants {}'.format(i.friendly_name, i.call_count()))
+        if i.codec_in_conference():
+            print('Codec Is Live')
+            # i.close()
+        else:
+            print('Codec Is off')
+
+
+
+
+if __name__ == '__main__':
+    main()
